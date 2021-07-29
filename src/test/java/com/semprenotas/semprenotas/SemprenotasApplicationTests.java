771442b909a52117eb4ebc4841d79867887ee778
unknown <diego.candido@dellead.com>
2021-07-28 21:01:20 -0300

    first commit

---
+++ src/test/java/com/semprenotas/semprenotas/SemprenotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semprenotas.semprenotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemprenotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
